ba3c7096d9372598b41e24ffbcbedb2817356b5c
jaloliddin.mahkamov@mail.ru <jaloliddin.mahkamov@mail.ru>
2024-04-05 20:14:59 +0500

    Add project files.

---
+++ Calculator/Program.cs
@@ -0,0 +1,7 @@
+﻿//1.Kalkulyator
+Console.Write("FirstNumber= ");
+double FirstNumber=double.Parse(Console.ReadLine());
+string operation=Console.ReadLine();
+Console.Write("SecondNumber= ");
+double SecondNumber = double.Parse(Console.ReadLine());
+
