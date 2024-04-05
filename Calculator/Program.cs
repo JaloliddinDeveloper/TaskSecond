19051490abc9de0af68cc5c0fefe7fdb31ad4325
jaloliddin.mahkamov@mail.ru <jaloliddin.mahkamov@mail.ru>
2024-04-05 20:39:00 +0500

    1. Kalkulyator,2. Raqamlar qatorining yig‘indisini hisoblash, 3.  Paritet tekshiruvi Project Initialize

--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -1,7 +1,50 @@
-﻿//1.Kalkulyator
-Console.Write("FirstNumber= ");
-double FirstNumber=double.Parse(Console.ReadLine());
-string operation=Console.ReadLine();
-Console.Write("SecondNumber= ");
-double SecondNumber = double.Parse(Console.ReadLine());
+﻿////1.Kalkulyator
+//Console.Write("FirstNumber= ");
+//double FirstNumber=double.Parse(Console.ReadLine());
+//Console.Write("Operation: ");
+//string operation=Console.ReadLine();
+//Console.Write("SecondNumber= ");
+//double SecondNumber = double.Parse(Console.ReadLine());
+
+//var result = operation switch
+//{
+//    "+" => $"{FirstNumber} + {SecondNumber} = {FirstNumber+SecondNumber}",
+//    "-" => $"{FirstNumber} - {SecondNumber} = {FirstNumber-SecondNumber}",
+//    "*" => $"{FirstNumber} * {SecondNumber} = {FirstNumber * SecondNumber}",
+//    "/"=> $"{FirstNumber} / {SecondNumber} = {FirstNumber / SecondNumber}",
+//};
+
+//Console.WriteLine(result);
+
+////2.Raqamlar qatorining yig‘indisini hisoblash
+//Console.Write("Enter N=");
+//int n=int.Parse(Console.ReadLine());
+//int s = 0,i=1;
+//while (i<=n)
+//{
+//    s=s+i;
+//    i++;
+//}
+//Console.WriteLine($"1 dan {n} gacha bo'lgan sonlar yig'indisi S={s} ga teng ekan");
+
+////3.Tub son tekshiruvi tekshiruvi
+
+//Console.Write("Ixtiyoriy natural son kiriting N=");
+//int n = int.Parse(Console.ReadLine());
+//int s = 0;
+//for (int i = 1; i <=n; i++)
+//{
+//    if (n%i==0)
+//    {
+//        s++;
+//    }
+//}
+//if (s == 2)
+//{
+//    Console.WriteLine($"Berilgan {n} soni tub son ekan ");
+//}
+//else
+//{
+//    Console.WriteLine($"Berilgan {n} soni tub son emas ekan ");
+//}
 
